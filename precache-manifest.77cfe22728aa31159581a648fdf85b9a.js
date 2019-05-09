09e556865b602b8a1b22e006429bb285b41646eb
adriangp <agonzalezpuig@amarello.com.mx>
2019-05-09 15:59:09 -0500

    Updates

---
+++ precache-manifest.77cfe22728aa31159581a648fdf85b9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "617e7a2ee1d860191c9d",
+    "url": "/dainar/coneapp/static/js/runtime~main.fc649616.js"
+  },
+  {
+    "revision": "7b8745e94f6e22602cdf",
+    "url": "/dainar/coneapp/static/js/main.29c2580b.chunk.js"
+  },
+  {
+    "revision": "a28e880255b1e23a0829",
+    "url": "/dainar/coneapp/static/js/2.f6543868.chunk.js"
+  },
+  {
+    "revision": "7b8745e94f6e22602cdf",
+    "url": "/dainar/coneapp/static/css/main.f7d7433d.chunk.css"
+  },
+  {
+    "revision": "9d1dc79bd3b8ffd16c77936483c8dbd4",
+    "url": "/dainar/coneapp/index.html"
+  }
+];

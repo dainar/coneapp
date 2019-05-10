b34a5cf7a66c2d86e82c3609a5a5ea46a2883f75
adriangp <agonzalezpuig@amarello.com.mx>
2019-05-09 21:17:40 -0500

    exp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute, Redirect, hashHistory } from 'react-router';
 import Error404 from './components/error.cmpt';
 
 import Login from './components/login.cmpt';
 import Register from './components/register.cmpt';
-import Resouces from './components/Resouces.cmpt';
+import Resources from './components/Resouces.cmpt';
 import Users from './components/users.cmpt';
 
 
 
 export default class App extends React.Component {
   componentDidMount(){
-    document.title = "Conektapp"
+    document.title = "Conekxtapp"
   }
     render() {
       
@@ -24,7 +24,7 @@ export default class App extends React.Component {
                   <IndexRoute component={Users}/>
                   <Route path="/login" component={Login}/>
               <Route path="/register" component={Register}/>
-                  <Route  path='/recursos' component={Resouces}/>
+                  <Route  path='/recursos' component={Resources}/>
                   <Route  path='/users'  component={Users}/> 
                   <Route  path="*"  component={Error404}/>
                 </Route>

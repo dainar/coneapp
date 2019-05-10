3a42261bff20403fcf8659b5590a21e2d0aa676e
adriangp <agonzalezpuig@amarello.com.mx>
2019-05-09 20:43:51 -0500

    react-create-app on Heroku2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router';
 import Error404 from './components/error.cmpt';
 
-
 import Login from './components/login.cmpt';
 import Register from './components/register.cmpt';
 import Resouces from './components/Resouces.cmpt';
@@ -15,19 +14,19 @@ export default class App extends React.Component {
     document.title = "Conektapp"
   }
     render() {
-      const baseUrl = process.env.PUBLIC_URL;
+      
         return (
           <div>
             <Router  history={browserHistory}>
             
             
-                <Route  path= {baseUrl + ''}>
+                <Route  path= '/'>
                   <IndexRoute component={Users}/>
-                  <Route path= {baseUrl + 'login'}  component={Login}/>
-              <Route path= {baseUrl + 'register'}  component={Register}/>
-                  <Route path= {baseUrl + 'recursos'} component={Resouces}/>
-                  <Route  path= {baseUrl + 'users'}  component={Users}/> 
-                  <Route  path= {baseUrl + '*'} component={Error404}/>
+                  <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+                  <Route  path='/recursos' component={Resouces}/>
+                  <Route  path='/users'  component={Users}/> 
+                  <Route  path="*"  component={Error404}/>
                 </Route>
             </Router>
           </div>

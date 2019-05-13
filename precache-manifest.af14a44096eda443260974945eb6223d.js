a1c3178ae349e54f61cf264edad656d70ca8dcf7
adriangp <agonzalezpuig@amarello.com.mx>
2019-05-13 17:03:04 -0500

    Updates

--- precache-manifest.af14a44096eda443260974945eb6223d.js
+++ precache-manifest.af14a44096eda443260974945eb6223d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/coneapp/static/js/runtime~main.58be0fba.js"
   },
   {
-    "revision": "62c54b06787bd1c8cbc5",
-    "url": "/coneapp/static/js/main.f57145da.chunk.js"
+    "revision": "dfe33fd51dbbcccf795d",
+    "url": "/coneapp/static/js/main.1137b3f6.chunk.js"
   },
   {
     "revision": "f86f59cf40c4d0c400a9",
     "url": "/coneapp/static/js/2.67c6c893.chunk.js"
   },
   {
-    "revision": "62c54b06787bd1c8cbc5",
+    "revision": "dfe33fd51dbbcccf795d",
     "url": "/coneapp/static/css/main.f7d7433d.chunk.css"
   },
   {
-    "revision": "b011d6035d0ef070e07fe75f392af6a5",
+    "revision": "88b49f1cd2ac3ed3d64efa640e53213b",
     "url": "/coneapp/index.html"
   }
 ];

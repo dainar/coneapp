b1dde1d6cf86fc4dac91df5b13563c89bfbd07e4
adriangp <agonzalezpuig@amarello.com.mx>
2019-05-13 22:40:23 -0500

    Updates

--- precache-manifest.b4019a7a9a0422648b4a6cdb8a9a7c9e.js
+++ precache-manifest.b4019a7a9a0422648b4a6cdb8a9a7c9e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/coneapp/static/js/runtime~main.58be0fba.js"
   },
   {
-    "revision": "cb7538b1b988c0d52bf5",
-    "url": "/coneapp/static/js/main.be26624a.chunk.js"
+    "revision": "dfefe66c93e38be2e3c9",
+    "url": "/coneapp/static/js/main.05d41f54.chunk.js"
   },
   {
     "revision": "f86f59cf40c4d0c400a9",
     "url": "/coneapp/static/js/2.67c6c893.chunk.js"
   },
   {
-    "revision": "cb7538b1b988c0d52bf5",
+    "revision": "dfefe66c93e38be2e3c9",
     "url": "/coneapp/static/css/main.f7d7433d.chunk.css"
   },
   {
-    "revision": "5d992cae9df73bbda1e84d4b80518b76",
+    "revision": "a49a407df1865a93d0ca6e1606df85e8",
     "url": "/coneapp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/coneapp/precache-manifest.23713c25c6fe2a9f62380e9ce6fb00c4.js"
+  "/coneapp/precache-manifest.b4019a7a9a0422648b4a6cdb8a9a7c9e.js"
 );
 
 workbox.clientsClaim();
